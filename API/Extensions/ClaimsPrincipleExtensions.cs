5338b7c9456c20235495475279cee8eabbe0d698
Nicolas Basiuk <nicolasbasiuk@gmail.com>
2021-04-12 01:27:25 +0300

    End of section 11

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,12 @@
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    static class ClaimsPrincipleExtensions
+    {
+        public static string GetUserName(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
